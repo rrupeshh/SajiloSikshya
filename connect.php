83c288b83f7aabfb321c2aadf9b28932990120c5
rrupeshh <rupesh.poudel07@gmail.com>
2018-09-21 23:18:24 +0545

    Added connect.php

---
+++ connect.php
@@ -0,0 +1,4 @@
+<?php 
+    $con = mysqli_connect("localhost","root","imatolymph") or die("Couldn't connect to the localhost");
+    mysqli_select_db($con,"sajilosikshya") or die("Couldn't connect to the database");   
+?>
