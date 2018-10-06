947a11f1f93d9d698301065f056f88f9906a9abf
rrupeshh <rupesh.poudel07@gmail.com>
2018-10-06 23:00:04 +0545

    Added edit

---
+++ edit_delete_news.php
@@ -0,0 +1,87 @@
+<?php 
+    session_start();
+    require_once('connect.php');
+    
+    $username = $_SESSION['username'];
+    
+    $errormessage="No Notifications";
+    
+    if(!$username) {
+        echo '<script type="text/javascript"> alert("Access Denied!"); </script>';
+        header('refresh:0;url=login.php');
+    }
+?>
+
+<?php 
+    include_once 'includes/header.php'; // Header of the page
+?>
+
+<?php 
+    if(!($_SESSION['role'])=='admin'){
+        header('location:login.php');
+    }
+?>
+
+<?php
+    include_once 'includes/information_section.php';
+?>
+
+<div id="main_section">
+    
+    <!-- Left Side Section -->
+    <div id="left_section" class="ss-card-2">
+        <div id="left_section_header">
+            Notification:
+        </div>
+        <div id="left_section_content">
+            <?php echo $errormessage; ?>
+        </div>
+    </div>
+    <!-- Left Side Section Ends Here -->
+    
+    <!-- Middle Section Starts Here -->
+    <div id="middle_section">
+        <div class="ss-article ss-card-8">
+            <div class="ss-article-header">
+                News!
+            </div>
+            <div class="ss-separator"></div>
+            <div class="ss-article-content">
+                <?php
+                    $sql1 = "SELECT * FROM news";
+                    $result1 = mysqli_query($con,$sql1);
+                    while ($row1=mysqli_fetch_array($result1)){
+                ?>
+                <div class="ss-container ss-card-4" style="margin-bottom: 10px;">
+                    <h4><?php echo $row1['news_headline']; ?></h4>
+                    <div class="ss-separator" style="margin-bottom: 5px;"></div>
+                    <?php echo $row1['news_short_description'] . "<br>"; ?>
+                    
+                    <a style="margin-right: 10px;text-decoration: none;float: right;color: #39f;border-left: 5px solid #39f;font-style:italic;padding-left: 10px;" href="edit_delete_news_func.php?id=<?php echo $row1['news_id']?>&function=edit">Edit...</a>
+                    <a style="margin-right: 10px;text-decoration: none;float: right;color: #39f;border-left: 5px solid #39f;font-style:italic;padding-left: 10px;" href="edit_delete_news_func.php?id=<?php echo $row1['news_id']?>&function=delete" onclick="return confirm('Are you sure want to delete this news?');">Delete...</a>
+                    
+                    <div class="ss-clear"></div>
+                </div>
+                <?php 
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+    <!-- Middle Section Ends Here -->
+    
+    <!-- Right Section Starts Here -->
+    <div id="right_section" class="ss-card-2">
+        <div id="right_section_header">
+            Side News
+        </div>
+        <div id="right_section_content">
+            <?php include('aside_news.php'); ?>
+        </div>
+    </div>
+    <!-- Right Section Ends Here -->
+</div>
+
+<?php
+    include_once 'includes/footer.php'; // Footer of the page
+?>
