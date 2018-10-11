76525d9aa9d350a949c9eb71df7eb905b626474b
rrupeshh <rupesh.poudel07@gmail.com>
2018-10-11 12:27:21 +0545

    Added information section

---
+++ includes/information_section.php
@@ -0,0 +1,5 @@
+<div id="information_section">
+    <div id="information_section_content">
+        You can build your skills ... We provide online exams to enhance your performance.
+    </div>
+</div>
